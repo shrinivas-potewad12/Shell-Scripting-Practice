fd2aec8a7eb78e157976d43ad6343af115233cf6
shrinivas <shrinivaspotewad@gmail.com>
2026-08-02 15:27:12 +0530

    09_if-else.sh file added

---
+++ 09_if-else.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+read -p "Enter your marks: " marks
+
+if [ $marks -gt 40 ]
+
+then
+
+	echo " Your are passed!!!"
+	echo " Second line"
+
+else
+	echo "You are Failed!!!!!!!!!!!!!!!!!!"
+
+
+
+fi
+
+
+
