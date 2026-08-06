7480da9993a54e98c4f6c43cbed826283f04d69d
shrinivas <shrinivaspotewad@gmail.com>
2026-08-06 15:43:49 +0530

    15_forloop.sh file added

---
+++ 15_forloop.sh
@@ -0,0 +1,24 @@
+
+
+
+#!/bin/bash
+
+for num in 10 20 30 40 50 
+do 
+
+	echo "Number is $num "
+
+done 
+
+
+for name in Raju Sham Paul Alex 
+do 
+	echo "Name is $name"
+
+done 
+
+
+for i in {1..10}
+do 
+	echo "$i"
+done 
